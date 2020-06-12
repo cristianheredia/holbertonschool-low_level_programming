edcb7def971c57170eeb80962cbc3fa168180d7d
Critian Heredia <2056@holbertonschool.com>
2020-06-12 06:11:07 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
-i!/bin/bash
 #include <stdio.h>
-#include <string.h>
-
-init main()
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	
-	char str [100]
-		;
-	puts(str, "Programming is like building a multilingual puzzle");
-	
-	puts(str);
-
-	return 0;	
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
