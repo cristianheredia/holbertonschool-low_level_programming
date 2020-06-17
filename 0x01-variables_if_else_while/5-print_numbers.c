47b4f58cf23176996f9ae19c30a1b284af9ef78b
Critian Heredia <2056@holbertonschool.com>
2020-06-17 15:38:34 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int contador
+		;
+	printf("\n");
+
+	contador = 0;
+
+	while (contador <= 9);
+	{
+		printf("%d ", contador);
+		contador++;
+	}
+
+	return (0);
+}
