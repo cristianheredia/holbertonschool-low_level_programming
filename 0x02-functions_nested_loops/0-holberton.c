77bcd9dc4028600e841b05f5520f6db62dfcdf26
Critian Heredia <2056@holbertonschool.com>
2020-07-14 20:27:17 +0000

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,12 @@
+int main(void)
+
+{
+        char c[10] = "Holberton";
+        for (int i = 0; i < 10; i++)
+
+        {
+                putchar (c[i]);
+        }
+        
+        return (0);
+}
